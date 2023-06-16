a70bc30397320ccace2b91c8094947aa3fef5722
gwanseeum <9v.veu.v@gmail.com>
2023-06-17 02:25:49 +0900

    Design: 문자 줄바꿈 수정

--- src/components/community/common/LeftSideNav.tsx
+++ src/components/community/common/LeftSideNav.tsx
@@ -6,38 +6,32 @@ import SideNavQuestion from 'assets/icons/SideNavQuestion.svg';
 import SideNavBoast from 'assets/icons/SideNavBoast.svg';
 
 function LeftSideNav() {
+  const navLinks = [
+    { to: '/community', label: '질문해요', image: `${SideNavQuestion}` },
+    { to: '/community/boast', label: '자랑해요', image: `${SideNavBoast}` },
+  ];
+
   return (
     <div>
       <aside className={styles.container}>
         <MyProfile />
         <nav>
           <ul>
-            <li>
-              <NavLink
-                to='/community'
-                className={({ isActive, isPending }) => (isPending ? styles.inactive : isActive ? styles.active : '')}
-              >
-                <div>
-                  <div className={styles.icon}>
-                    <img src={SideNavQuestion} alt='' />
+            {navLinks.map(({ to, label, image }) => (
+              <li key={label}>
+                <NavLink
+                  to={to}
+                  className={({ isActive, isPending }) => (isPending ? styles.inactive : isActive ? styles.active : '')}
+                >
+                  <div>
+                    <div className={styles.icon}>
+                      <img src={image} alt='' />
+                    </div>
+                    <span>{label}</span>
                   </div>
-                  <span>질문해요</span>
-                </div>
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to='/community/boast'
-                className={({ isActive, isPending }) => (isPending ? styles.inactive : isActive ? styles.active : '')}
-              >
-                <div>
-                  <div className={styles.icon}>
-                    <img src={SideNavBoast} alt='' />
-                  </div>
-                  <span>자랑해요</span>
-                </div>
-              </NavLink>
-            </li>
+                </NavLink>
+              </li>
+            ))}
           </ul>
         </nav>
       </aside>
